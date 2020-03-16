b5efed146831f83683d9d7085ff7b3e1a92c1d85
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-16 18:01:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog
-  def .name=(dog_name)
+  def name=(dog_name)
     @this_dogs_name = dog_name 
   end
   
-  def .name
+  def name
     @this_dogs_name
   end
   
-  def .bark
+  def bark
     puts "woof!"
   end
 end
